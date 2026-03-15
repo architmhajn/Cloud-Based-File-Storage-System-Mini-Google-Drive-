66436f619f47a6060105dfe4ab1e35c3900bd081
Archit Mahajan <mahajanarchit83@gmail.com>
2026-03-16 02:30:01 +0530

    fixed upload folder error and UI improvements

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ UPLOAD_FOLDER = "uploads"
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
+st.sidebar.title("📁 Dashboard")
+st.sidebar.write("Mini Google Drive")
+st.sidebar.write("Manage your files easily")
+
+file_count = len(os.listdir(UPLOAD_FOLDER))
+st.sidebar.metric("Total Files", file_count)
+
 st.title("☁ Mini Google Drive")
 
 uploaded_file = st.file_uploader("Upload a file")
@@ -25,14 +32,20 @@ files = os.listdir(UPLOAD_FOLDER)
 for file in files:
     col1, col2, col3 = st.columns(3)
 
+    file_path = os.path.join(UPLOAD_FOLDER, file)
+
     with col1:
-        st.write(file)
+        size = os.path.getsize(file_path) / 1024
+        st.write(f"{file} ({size:.2f} KB)")
 
     with col2:
-        with open(os.path.join(UPLOAD_FOLDER, file), "rb") as f:
+        if file.endswith(("png","jpg","jpeg")):
+            st.image(file_path, width=150)
+
+        with open(file_path, "rb") as f:
             st.download_button("Download", f, file_name=file)
 
     with col3:
         if st.button("Delete", key=file):
-            os.remove(os.path.join(UPLOAD_FOLDER, file))
+            os.remove(file_path)
             st.rerun()
